7b05aefd5efda06406d43f31265b0c22296e8b3c
vasanth-exalogic <vasanthakumar296@gmail.com>
2020-07-21 20:55:47 +0530

    Initial Commit

---
+++ db/migrate/20200721151640_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+      t.string :name
+      t.string :comment
+      t.integer :post_id
+
+      t.timestamps
+    end
+  end
+end
