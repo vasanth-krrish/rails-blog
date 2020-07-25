887d21fb3c5605b0e26dd8d981ec376f9e878dda
vasanth-krrish <vasanthkrrish96@gmail.com>
2020-07-25 10:50:01 +0530

    minor changes in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,7 @@ class User < ApplicationRecord
     self.add_role(:user) if self.roles.blank?
   end
 
-  has_many :posts
-  has_many :comments, through: :posts
-
-  
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
 end
